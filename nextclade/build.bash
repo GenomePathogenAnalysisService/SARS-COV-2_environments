9558462285223059540611cd0ab0c80182af7916
Jez Swann <Jeremy.Swann@ndm.ox.ac.uk>
2021-11-24 09:18:14 +0000

    Updated build.bash for nextclade to use non-depreceated repo like CI/CD

--- nextclade/build.bash
+++ nextclade/build.bash
@@ -1,3 +1,3 @@
-sudo docker run  -v /var/run/docker.sock:/var/run/docker.sock  -v /home/nick/soft/nextclade/sing_images:/output  --privileged -t --rm  singularityware/docker2singularity neherlab/nextclade
+sudo docker run  -v /var/run/docker.sock:/var/run/docker.sock  -v /home/nick/soft/nextclade/sing_images:/output  --privileged -t --rm  singularityware/docker2singularity nextstrain/nextclade
 
-sudo singularity build /mnt/nanostore/soft/images/nextclade_$(date +%F_%T).simg docker:neherlab/nextclade
+sudo singularity build /mnt/nanostore/soft/images/nextclade_$(date +%F_%T).simg docker:nextstrain/nextclade
